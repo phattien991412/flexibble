6d3af6846169e82835bf5ab72c651759d798b0cd
Phat Tien <phattien991412@gmail.com>
2023-08-03 23:12:33 +0700

    fix bug undefined variable

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,13 @@ import LoadMore from "@/components/LoadMore";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/action";
 
+type SearchParams = {
+  category?: string | null;
+  endcursor?: string | null;
+}
+
 type Props = {
-  category?: string;
-  endcursor?: string;
+  searchParams: SearchParams
 }
 
 type ProjectSearch = {
@@ -25,7 +29,7 @@ export const dynamic = 'force-dynamic';
 export const dynamicParams = true;
 export const revalidate = 0;
 
-const Home = async ({  category, endcursor  }: Props) => {
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
   const data = await fetchAllProjects(category || "", endcursor) as ProjectSearch
   const projectsToDisplay = data?.projectSearch?.edges || [];
 
